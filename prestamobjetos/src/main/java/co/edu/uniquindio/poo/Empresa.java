ddb5d19507dfd0382e7f9dde717de2876b7b4af6
Juan Pablo López <juanp.lopezg@uqvirtual.edu.co>
2023-10-08 13:38:29 -0500

    Se realizó casi toda la base del programa

---
+++ prestamobjetos/src/main/java/co/edu/uniquindio/poo/Empresa.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.poo;
+
+import java.util.Collection;
+
+public class Empresa {
+    public String nombre;
+    private String nit;
+    private final Collection<Cliente> listaClientes;
+    private final Collection<Empleado> listaEmpleados;
+    private final Collection<Prestamo> listaPrestamos;
+    private final Collection<Objeto> listaObjetos;
+}
